d64b117bad25e8f374f87368c0d9fc79dc912be2
ShivamDashore16 <shivamdashore16@gmail.com>
2022-05-23 18:26:59 +0530

    Multiply class commited

---
+++ src/com/Test/GitMultiply.java
@@ -0,0 +1,16 @@
+package com.Test;
+
+public class GitMultiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20;
+		int b=70;
+		int c=a*b;
+		
+		System.out.println("Multiply of two numbers is "+c);
+		
+	}
+
+}
