9ec3b29522ea0386bf054371dfa562cfb512ef99
ShivamDashore16 <shivamdashore16@gmail.com>
2022-05-23 18:02:55 +0530

    Sum class commited

---
+++ src/com/Test/GitSum.java
@@ -0,0 +1,16 @@
+package com.Test;
+
+public class GitSum {
+
+	public static void main(String[] args) {
+		
+		int a=80;
+		int b=70;
+		int c=a+b;
+		
+		System.out.println("Addition of numbers is "+c);
+		
+
+	}
+
+}
